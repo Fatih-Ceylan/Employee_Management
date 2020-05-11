f07cd9db2202e7ed413a59baabbe5bd46ca53f1d
Fatih Ceylan <54042756+Fatih-Ceylan@users.noreply.github.com>
2020-05-11 16:24:57 +0300

    Add files via upload

---
+++ Telefon_Rehberi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Telefon_Rehberi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Telefon_Rehberi/Models/SQLEmployeeRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telefon_Rehberi.Controllers;
+
+namespace Telefon_Rehberi.Models
+{
+    public class SQLEmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext context;
+
+        public SQLEmployeeRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public Employee Add(Employee employee)
+        {
+            context.Employees.Add(employee);
+            context.SaveChanges();
+            return employee;
+        }
+
+        public Employee Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
