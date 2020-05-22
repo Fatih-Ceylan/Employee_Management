be6582df1374085812758de64e89f713e18fc4e5
Fatih Ceylan <54042756+Fatih-Ceylan@users.noreply.github.com>
2020-05-22 21:27:56 +0300

    Add files via upload

---
+++ ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+namespace Telefon_Rehberi.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        [Required]
+        [MaxLength(20, ErrorMessage = "İsim 20 karakterden fazla olamaz")]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
---
+++ ViewModels/HomeDetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Telefon_Rehberi.ViewModels
+{
+    public class HomeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
