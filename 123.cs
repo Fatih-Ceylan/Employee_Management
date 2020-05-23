b088e138433fbdd326dde573df2e143922f760e7
Fatih Ceylan <fceylan001@gmail.com>
2020-05-23 19:10:28 +0300

    Create 123.cs

---
+++ 123.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telefon_Rehberi
+{
+    public class _123
+    {
+        //aasd
+    }
+}
