b35d36f4f849d021d7e619301d607b606784a5c1
Fatih Ceylan <fceylan001@gmail.com>
2020-06-21 16:25:30 +0300

    AccountLockout
    set the timer lockout  for 15 mins after 5 incorrect password enter

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -5,18 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry page not found";
+                    logger.LogWarning($"404 Error Occured. Path={statusCodeResult.OriginalPath} and QueryString ={statusCodeResult.OriginalQueryString}");
                     break;
                 case 405:
                     ViewBag.ErrorMessage = "Sorry page not found";
@@ -31,9 +41,10 @@ namespace EmployeeManagement.Controllers
         {
             var exDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
 
-            ViewBag.expath = exDetails.Path;
-            ViewBag.msg = exDetails.Error.Message;
-            ViewBag.Trace = exDetails.Error.StackTrace;
+            logger.LogError($"The path{exDetails.Path} threw and exception{exDetails.Error}");
+            //ViewBag.expath = exDetails.Path;
+            //ViewBag.msg = exDetails.Error.Message;
+            //ViewBag.Trace = exDetails.Error.StackTrace;
 
             return View("Error");
         }
