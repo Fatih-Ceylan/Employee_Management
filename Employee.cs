8f00202d4692863101d904261f3968ae0be2b2fb
Fatih Ceylan <fceylan001@gmail.com>
2021-11-10 18:27:17 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagement
+{
+    public class Employee
+    {
+
+        // git test comment
+        public int Id { get; set; }
+        [NotMapped]
+        public string EncryptedId { get; set; }
+        [Required]
+        [MaxLength(10 , ErrorMessage = "İsim 10 karakterden fazla olamaz")]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
