05997db5b0732b9c884a65fcf7dd8accc13a9a4e
Fatih Ceylan <fceylan001@gmail.com>
2021-12-09 16:40:32 +0300

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -9,6 +9,7 @@ namespace EmployeeManagement.Controllers
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        
         }
         public DbSet<Employee> Employees { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
