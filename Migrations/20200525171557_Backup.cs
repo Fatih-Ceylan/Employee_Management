3169120e54e9defee031895ddcc052ee3dde87ab
Fatih Ceylan <fceylan001@gmail.com>
2021-12-09 16:40:57 +0300

    Update 20200525171557_Backup.cs

--- Migrations/20200525171557_Backup.cs
+++ Migrations/20200525171557_Backup.cs
@@ -6,6 +6,7 @@ namespace EmployeeManagement.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+        
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
